2f2e2160ae9f8bfeba63c39f305f764cc5517db8
Artem Leushin <benreily2016@gmail.com>
2018-08-31 08:59:46 +0300

    -fixed compile issues

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -19,10 +19,10 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = false;
     public static int BUILD_VERSION = 1358;
     public static String BUILD_VERSION_STRING = "4.9.1";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int APP_ID = 443115; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "b188c2da277b3918e627dbb624dca1bc"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "6c6a1b960d3948ce8b704cce72cf5372";
+    public static String HOCKEY_APP_HASH_DEBUG = "6c6a1b960d3948ce8b704cce72cf5372";
     public static String PLAYSTORE_APP_URL = "";
 
     static {
